17c55c1aa2cd259e950d8bffef7324413d4622e2
joseManuel540 <josenoob312@gmail.com>
2026-03-14 20:37:07 -0500

    archivosEj2

---
+++ Gestor de Contactos Simple archivoEj2/Gestor de Contactos Simple archivoEj2/Clases/Contacto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Gestor_de_Contactos_Simple_archivoEj2.Clases
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public string ToCSV() => $"{Nombre};{Telefono};{Correo}";
+
+        public static void Ejecutar()
+        {
+            Contacto contacto = new Contacto();
+
+            Console.WriteLine("ingrese un nombre");
+            contacto.Nombre = Console.ReadLine().Trim();
+            Console.WriteLine("Ingrese su telefono");
+            contacto.Telefono = int.Parse(Console.ReadLine().Trim());
+            Console.WriteLine("Ingrese su correo");
+            contacto.Correo = Console.ReadLine().Trim().ToLower();
+
+            string ruta = "contactos.csv";
+            File.AppendAllText(ruta, contacto.ToCSV() + Environment.NewLine);
+
+            Console.WriteLine("Deseas listar contactos? (si/no)");
+            string rta = Console.ReadLine().Trim().ToLower();
+
+            if (rta == "si")
+            {
+                if (File.Exists(ruta))
+                {
+                    Console.WriteLine("\nCONTACTOS");
+                    Console.WriteLine("------------------------------------------------------------------------------------");
+
+                    string[] lineas = File.ReadAllLines(ruta);
+
+                    foreach (string linea in lineas)
+                    {
+                        string[] datos = linea.Split(';');
+
+                        Console.WriteLine($"Nombre: {datos[0]}  Tel: {datos[1]}  Correo: {datos[2]}");
+                    }
+                }
+
+            }
+        }
+    }
+
+}   
---
+++ Gestor de Contactos Simple archivoEj2/Gestor de Contactos Simple archivoEj2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Gestor de Contactos Simple (Formato CSV)
+Objetivo: Aplicar el concepto de datos estructurados para guardar una pequeña agenda de 
+contactos.
+Instrucciones:
+Crea una clase Contacto con las propiedades: Nombre, Telefono y Correo.
+Solicita estos tres datos al usuario por teclado.
+Implementa un método ToCSV() en la clase que devuelva los datos separados por punto y coma (;).
+Guarda el contacto en un archivo contactos.csv.
+Validación: Antes de guardar, usa String.Trim() y String.ToLower() para normalizar el correo 
+electrónico.
+Al finalizar, pregunta al usuario si desea "Listar contactos". Si dice que sí, lee el archivo,
+haz un Split(';') y muestra los datos en una tabla limpia en la consola.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gestor_de_Contactos_Simple_archivoEj2.Clases;
+
+namespace Gestor_de_Contactos_Simple_archivoEj2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacto.Ejecutar();
+        }
+    }
+}
