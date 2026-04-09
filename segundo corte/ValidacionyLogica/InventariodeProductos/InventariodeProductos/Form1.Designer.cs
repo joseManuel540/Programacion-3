83f3ed7b3a376e6d848d5523c871c4f644a37c8a
joseManuel540 <josenoob312@gmail.com>
2026-04-08 20:11:53 -0500

    ejercicio 2

--- segundo corte/ValidacionyLogica/InventariodeProductos/InventariodeProductos/Form1.Designer.cs
+++ segundo corte/ValidacionyLogica/InventariodeProductos/InventariodeProductos/Form1.Designer.cs
@@ -35,13 +35,19 @@
             this.numStockInicial = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rbExento = new System.Windows.Forms.RadioButton();
-            this.rbGeneral = new System.Windows.Forms.RadioButton();
             this.rbReducido = new System.Windows.Forms.RadioButton();
+            this.rbGeneral = new System.Windows.Forms.RadioButton();
+            this.rbExento = new System.Windows.Forms.RadioButton();
             this.chkEsPerecedero = new System.Windows.Forms.CheckBox();
             this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
             this.numStockMinimo = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.lblIVA = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblValidar = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numStockInicial)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numStockMinimo)).BeginInit();
@@ -118,16 +124,16 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "IVA";
             // 
-            // rbExento
+            // rbReducido
             // 
-            this.rbExento.AutoSize = true;
-            this.rbExento.Location = new System.Drawing.Point(20, 58);
-            this.rbExento.Name = "rbExento";
-            this.rbExento.Size = new System.Drawing.Size(75, 33);
-            this.rbExento.TabIndex = 0;
-            this.rbExento.TabStop = true;
-            this.rbExento.Text = "0%";
-            this.rbExento.UseVisualStyleBackColor = true;
+            this.rbReducido.AutoSize = true;
+            this.rbReducido.Location = new System.Drawing.Point(20, 188);
+            this.rbReducido.Name = "rbReducido";
+            this.rbReducido.Size = new System.Drawing.Size(75, 33);
+            this.rbReducido.TabIndex = 2;
+            this.rbReducido.TabStop = true;
+            this.rbReducido.Text = "5%";
+            this.rbReducido.UseVisualStyleBackColor = true;
             // 
             // rbGeneral
             // 
@@ -140,16 +146,16 @@
             this.rbGeneral.Text = "19%";
             this.rbGeneral.UseVisualStyleBackColor = true;
             // 
-            // rbReducido
+            // rbExento
             // 
-            this.rbReducido.AutoSize = true;
-            this.rbReducido.Location = new System.Drawing.Point(20, 188);
-            this.rbReducido.Name = "rbReducido";
-            this.rbReducido.Size = new System.Drawing.Size(75, 33);
-            this.rbReducido.TabIndex = 2;
-            this.rbReducido.TabStop = true;
-            this.rbReducido.Text = "5%";
-            this.rbReducido.UseVisualStyleBackColor = true;
+            this.rbExento.AutoSize = true;
+            this.rbExento.Location = new System.Drawing.Point(20, 58);
+            this.rbExento.Name = "rbExento";
+            this.rbExento.Size = new System.Drawing.Size(75, 33);
+            this.rbExento.TabIndex = 0;
+            this.rbExento.TabStop = true;
+            this.rbExento.Text = "0%";
+            this.rbExento.UseVisualStyleBackColor = true;
             // 
             // chkEsPerecedero
             // 
@@ -189,11 +195,89 @@
             this.label4.TabIndex = 10;
             this.label4.Text = "Stock inicial";
             // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(534, 296);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(150, 116);
+            this.btnValidar.TabIndex = 11;
+            this.btnValidar.Text = "VALIDAR";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.ForeColor = System.Drawing.Color.Red;
+            this.lblCodigo.Location = new System.Drawing.Point(58, 128);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(220, 20);
+            this.lblCodigo.TabIndex = 12;
+            this.lblCodigo.Text = "Debe tener prefijo: PROD-";
+            this.lblCodigo.Visible = false;
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStock.ForeColor = System.Drawing.Color.Red;
+            this.lblStock.Location = new System.Drawing.Point(602, 128);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(368, 20);
+            this.lblStock.TabIndex = 13;
+            this.lblStock.Text = "El stock inicial no puede ser menor al minimo";
+            this.lblStock.Visible = false;
+            // 
+            // lblIVA
+            // 
+            this.lblIVA.AutoSize = true;
+            this.lblIVA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIVA.ForeColor = System.Drawing.Color.Red;
+            this.lblIVA.Location = new System.Drawing.Point(74, 426);
+            this.lblIVA.Name = "lblIVA";
+            this.lblIVA.Size = new System.Drawing.Size(331, 20);
+            this.lblIVA.TabIndex = 14;
+            this.lblIVA.Text = "Porfavor seleccione almenos una opcion";
+            this.lblIVA.Visible = false;
+            this.lblIVA.Click += new System.EventHandler(this.lblIVA_Click);
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.ForeColor = System.Drawing.Color.Red;
+            this.lblCategoria.Location = new System.Drawing.Point(364, 128);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(212, 20);
+            this.lblCategoria.TabIndex = 15;
+            this.lblCategoria.Text = "Seleccione una categoria";
+            this.lblCategoria.Visible = false;
+            // 
+            // lblValidar
+            // 
+            this.lblValidar.AutoSize = true;
+            this.lblValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValidar.ForeColor = System.Drawing.Color.Lime;
+            this.lblValidar.Location = new System.Drawing.Point(530, 426);
+            this.lblValidar.Name = "lblValidar";
+            this.lblValidar.Size = new System.Drawing.Size(243, 20);
+            this.lblValidar.TabIndex = 17;
+            this.lblValidar.Text = "producto agregado con exito!";
+            this.lblValidar.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 637);
+            this.Controls.Add(this.lblValidar);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblIVA);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.btnValidar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.numStockMinimo);
             this.Controls.Add(this.dtpFechaVencimiento);
@@ -233,6 +317,12 @@
         private System.Windows.Forms.DateTimePicker dtpFechaVencimiento;
         private System.Windows.Forms.NumericUpDown numStockMinimo;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Label lblIVA;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblValidar;
     }
 }
 
--- segundo corte/ValidacionyLogica/InventariodeProductos/InventariodeProductos/Form1.cs
+++ segundo corte/ValidacionyLogica/InventariodeProductos/InventariodeProductos/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -30,10 +31,72 @@ namespace InventariodeProductos
 
         private void txtCodigo_TextChanged(object sender, EventArgs e)
         {
-            if (true)
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string codigo = txtCodigo.Text;
+            bool IsValid = true;
+
+            if (!Regex.IsMatch(codigo, "^PROD-")) //^ SE usa para indicar al inicio del texto
             {
-                
+                lblCodigo.Visible = true;
+                IsValid = false;
             }
+            else
+            {
+                lblCodigo.Visible = false;
+            }
+
+            if (cmCategoria.Text == "")
+            {
+                lblCategoria.Visible = true;
+                IsValid = false;
+            }
+            else
+            {
+                lblCategoria.Visible= false;
+            }
+
+            if (numStockInicial.Value < numStockMinimo.Value)
+            {
+                lblStock.Visible = true;
+                IsValid = false;
+            }
+            else
+            {
+                lblStock.Visible = false;
+            }
+
+            if (chkEsPerecedero.Checked)
+            {
+                dtpFechaVencimiento.Enabled = true;
+            }
+
+            if (!rbExento.Checked && !rbGeneral.Checked && !rbReducido.Checked)
+            {
+                lblIVA.Visible = true;
+                IsValid = false;
+            }
+            else
+            {
+                lblIVA.Visible = false;
+            }
+
+            if (IsValid)
+            {
+                lblValidar.Visible = true;
+            }
+            else
+            {
+                lblValidar.Visible = false;
+            }
+
+        }
+
+        private void lblIVA_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
