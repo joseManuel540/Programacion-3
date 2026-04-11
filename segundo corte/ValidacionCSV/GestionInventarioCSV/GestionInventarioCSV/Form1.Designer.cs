9df544273a7396efc7dd021c7a50e4eaa7df3a7d
joseManuel540 <josenoob312@gmail.com>
2026-04-11 17:28:13 -0500

    ejercicio 1

---
+++ segundo corte/ValidacionCSV/GestionInventarioCSV/GestionInventarioCSV/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace GestionInventarioCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.numCantidad = new System.Windows.Forms.NumericUpDown();
+            this.numCosto = new System.Windows.Forms.NumericUpDown();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCargarProductos = new System.Windows.Forms.Button();
+            this.listProductos = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCosto)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCrear);
+            this.groupBox1.Controls.Add(this.numCantidad);
+            this.groupBox1.Controls.Add(this.numCosto);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(27, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(957, 352);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Crear producto";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(27, 295);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(204, 57);
+            this.btnCrear.TabIndex = 9;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // numCantidad
+            // 
+            this.numCantidad.Location = new System.Drawing.Point(749, 216);
+            this.numCantidad.Name = "numCantidad";
+            this.numCantidad.Size = new System.Drawing.Size(120, 35);
+            this.numCantidad.TabIndex = 8;
+            // 
+            // numCosto
+            // 
+            this.numCosto.Location = new System.Drawing.Point(412, 216);
+            this.numCosto.Name = "numCosto";
+            this.numCosto.Size = new System.Drawing.Size(120, 35);
+            this.numCosto.TabIndex = 7;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(27, 215);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(213, 35);
+            this.txtCodigo.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(27, 94);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(858, 35);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Codigo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Costo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(744, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cantidad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCargarProductos);
+            this.groupBox2.Controls.Add(this.listProductos);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(27, 412);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(957, 226);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listado Productos";
+            // 
+            // btnCargarProductos
+            // 
+            this.btnCargarProductos.Location = new System.Drawing.Point(27, 191);
+            this.btnCargarProductos.Name = "btnCargarProductos";
+            this.btnCargarProductos.Size = new System.Drawing.Size(192, 35);
+            this.btnCargarProductos.TabIndex = 1;
+            this.btnCargarProductos.Text = "Actualizar";
+            this.btnCargarProductos.UseVisualStyleBackColor = true;
+            this.btnCargarProductos.Click += new System.EventHandler(this.btnCargarProductos_Click);
+            // 
+            // listProductos
+            // 
+            this.listProductos.FormattingEnabled = true;
+            this.listProductos.ItemHeight = 29;
+            this.listProductos.Location = new System.Drawing.Point(27, 35);
+            this.listProductos.Name = "listProductos";
+            this.listProductos.Size = new System.Drawing.Size(914, 149);
+            this.listProductos.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 640);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCosto)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.NumericUpDown numCantidad;
+        private System.Windows.Forms.NumericUpDown numCosto;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listProductos;
+        private System.Windows.Forms.Button btnCargarProductos;
+    }
+}
+
---
+++ segundo corte/ValidacionCSV/GestionInventarioCSV/GestionInventarioCSV/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionInventarioCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validar que los campos no estén vacíos
+                if (string.IsNullOrWhiteSpace(txtNombre.Text) || string.IsNullOrWhiteSpace(txtCodigo.Text))
+                {
+                    MessageBox.Show("Por favor complete todos los campos de texto.", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Nombre del archivo CSV
+                string nombreArchivo = "productos.csv";
+                string rutaArchivo = Path.Combine(Application.StartupPath, nombreArchivo);
+
+                // Si el archivo no existe, crear el encabezado
+                if (!File.Exists(rutaArchivo))
+                {
+                    File.WriteAllText(rutaArchivo, "Codigo,Nombre,Costo,Cantidad" + Environment.NewLine, Encoding.UTF8);
+                }
+
+                // Agregar la información del producto
+                string linea = $"{txtCodigo.Text},{txtNombre.Text},{numCosto.Value},{numCantidad.Value}" + Environment.NewLine;
+                File.AppendAllText(rutaArchivo, linea, Encoding.UTF8);
+
+                MessageBox.Show($"Producto guardado exitosamente en {nombreArchivo}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Limpiar los campos después de guardar
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar el producto: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnCargarProductos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Nombre del archivo CSV
+                string nombreArchivo = "productos.csv";
+                string rutaArchivo = Path.Combine(Application.StartupPath, nombreArchivo);
+
+                // Verificar si el archivo existe
+                if (!File.Exists(rutaArchivo))
+                {
+                    MessageBox.Show("No se encontró el archivo de productos.", "Archivo no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                // Limpiar la lista antes de cargar
+                listProductos.Items.Clear();
+
+                // Leer todas las líneas del archivo
+                string[] lineas = File.ReadAllLines(rutaArchivo, Encoding.UTF8);
+
+                // Procesar cada línea (saltar la primera línea que son los encabezados)
+                for (int i = 1; i < lineas.Length; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(lineas[i]))
+                    {
+                        string[] campos = lineas[i].Split(',');
+                        if (campos.Length >= 4)
+                        {
+                            string codigo = campos[0].Trim();
+                            string nombre = campos[1].Trim();
+                            string costo = campos[2].Trim();
+                            string cantidad = campos[3].Trim();
+
+                            // Concatenar toda la información del producto
+                            string productoCompleto = $"[{codigo}] {nombre} - Costo: ${costo} - Cantidad: {cantidad}";
+                            listProductos.Items.Add(productoCompleto);
+                        }
+                    }
+                }
+
+                MessageBox.Show($"Se cargaron {listProductos.Items.Count} productos.", "Carga exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los productos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Clear();
+            txtCodigo.Clear();
+            numCosto.Value = 0;
+            numCantidad.Value = 0;
+        }
+    }
+}
